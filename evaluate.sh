64f1a581496a6684c63472cdf4ec958bc4beb9e8
vincent letard <vincent.letard@limsi.fr>
2015-08-06 15:51:39 +0200

    Improving automated evaluation

--- evaluate.sh
+++ evaluate.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
-if (( $# != 1 ))
+if (( $# <= 1 ))
 then
-  echo "Entrer l'adresse du fichier de test (requêtes)" >&2
+  echo "Test file(s) missing (requests)" >&2
   exit
 fi
 
-file=$(echo $1 | rev | cut -f1 -d"/" | rev)
-root=$(echo $1 | rev | cut -f2- -d"/" | rev)
-
-for strategy in intra inter both singletons
+while [[ "$1" != "" ]]
 do
-  time ./main.lua $strategy < $1 > $root/results_${file}_$strategy
+
+  file=$(echo $1 | rev | cut -f1 -d"/" | rev)
+  #root=$(echo $1 | rev | cut -f2- -d"/" | rev)
+  root=/tmp/
+  
+  overwrite=false
+  
+  read -p "Name of the case base version: " name
+  
+  for strategy in intra inter both singletons
+  do
+    output_file="$root/results_cb${name}_${file}_$strategy"
+    if test -f "$output_file" && ! $overwrite
+    then
+      read -sn 1 -t 10 -p "Output file $output_file already exists, are you sure you want to overwrite it ? (y)es/(n)o/(a)lways/(C)ancel" ans
+      echo
+      if (( $? == 142 ))
+      then
+        ans=C
+      fi
+      case $ans in
+        "y"|"Y") echo "overwriting" ;;
+        "n"|"N") echo "skipping file"; continue ;;
+        "a"|"A") echo "overwriting all files"; overwrite=true ;;
+        *) echo "aborting"; break ;;
+      esac
+    fi
+    time ./main.lua $strategy < $1 > $output_file
+  done
+
+  shift
 done
